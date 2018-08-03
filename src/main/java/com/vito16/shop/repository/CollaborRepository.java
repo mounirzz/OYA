35a9ac8d3a3a6bc85dd6829ec27acc97a4063cef
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-08-03 15:37:22 +0100

    documents

--- src/main/java/com/vito16/shop/repository/CollaborRepository.java
+++ src/main/java/com/vito16/shop/repository/CollaborRepository.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.vito16.shop.model.Collaborateur;
 @Repository
 public interface CollaborRepository extends JpaRepository<Collaborateur, Integer>{
-
-
 	public Collaborateur findByUsername(String username);
-
 	public Collaborateur findByUsernameAndPassword(String username, String password);
 
 }
